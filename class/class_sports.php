5fd34d58a862c1b3bd390be6f1d9e56c0fd605d8
MohamedMoustir <itsmoustir@gmail.com>
2024-12-25 16:01:37 +0100

    creattion class users et clinet

---
+++ class/class_sports.php
@@ -0,0 +1,42 @@
+<?php
+
+ class users 
+{
+
+    public $username;
+    public $email ;
+    private $password;
+    private $Role; 
+
+    function getuser(){
+
+    }
+    
+    function setuser(){
+        
+    }
+
+    function getpassword(){
+        
+    }
+
+    function setpassword(){
+        
+    }
+
+    function getrole(){
+        
+    } 
+    function setrole(){
+        
+    }
+
+}
+
+ class Clinet{
+ 
+ }
+ class admin{
+
+ }
+
