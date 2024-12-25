80d944091673504f8bd447f9b5bf919ef881f191
hichamoubaha <oubahahicham7@gmail.com>
2024-12-25 15:45:40 +0100

    ajouter la partie de l'insertion sur la table activites

--- conception/script.sql
+++ conception/script.sql
@@ -12,3 +12,15 @@ CREATE TABLE `activites` (
   `Disponibilite` tinyint DEFAULT '0',
   `image_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--Déchargement des données de la table `activites`
+
+INSERT INTO `activites` (`id_activite`, `Nom_activite`, `Description_activite`, `Capacite`, `date_debut`, `date_fin`, `Disponibilite`, `image_path`) VALUES
+(156, 'Zumba', 'Cours de zumba énergétiques', 30, '2024-02-01', '2024-11-30', 1, 'images/zumba.jpg'),
+(157, 'Musculation', 'Entraînement pour la musculation', 15, '2024-03-01', '2024-10-31', 1, 'images/musculation.jpg'),
+(158, 'Pilates', 'Cours de Pilates pour le bien-être', 25, '2024-01-15', '2024-09-15', 1, 'images/pilates.jpg'),
+(159, 'Basketball', 'Matchs de basketball en équipe', 10, '2024-04-01', '2024-08-31', 0, 'images/basketball.jpg'),
+(160, 'Boxe', 'Entraînement de boxe pour tous les niveaux', 18, '2024-02-01', '2024-12-01', 1, 'images/boxe.jpg'),
+(161, 'Football', 'Compétitions de football en salle', 12, '2024-05-01', '2024-10-30', 1, 'images/football.jpg'),
+(163, 'Tennis', 'Cours de tennis pour débutants et avancés', 8, '2024-06-01', '2024-11-30', 1, 'images/tennis.jpg'),
+(164, 'Danse classique', 'Séances de danse classique', 20, '2024-01-20', '2024-10-01', 1, 'images/danse_classique.jpg');
