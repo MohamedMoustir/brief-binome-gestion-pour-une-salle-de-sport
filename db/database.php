676fd87b0369ee586cfdd643e3ab65ca550479e6
MohamedMoustir <itsmoustir@gmail.com>
2024-12-26 10:23:07 +0100

    coniextion database

---
+++ db/database.php
@@ -0,0 +1,40 @@
+<?php
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASSWORD', 'root');
+define('DATABASE', 'all_sports');
+
+class Database
+{
+    private $host = HOST;
+    private $username = USER;
+    private $password = PASSWORD;
+    private $database = DATABASE;
+    private $pdo;
+    public $error;
+
+    public function __construct()
+    {
+        $this->db_connect();
+    }
+
+    private function db_connect()
+    {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->database};charset=utf8mb4";
+            $this->pdo = new PDO($dsn, $this->username, $this->password);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->error = "Connection successful.";
+            echo $this->error;
+        } catch (PDOException $e) {
+            $this->error = "Connection failed: " . $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+    public function getPdo()
+    {
+        return $this->pdo;
+    }
+}
+?>
